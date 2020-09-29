ed99a78f70e36aefe5efe2cff966649625829f88
Ricardohll <71719429+Ricardohll@users.noreply.github.com>
2020-09-29 20:00:01 +0200

    commit final geometry
    no terminado aun la geometry

--- TEST1/TEST1/Program.cs
+++ TEST1/TEST1/Program.cs
@@ -6,7 +6,7 @@ namespace TEST1
     {
         static void Main(string[] args)
         {
-            double s1, s2;
+            double s1, s2, vector;
 
             s1 = SquateSolution.SolvedEcuation(0.1, 2.0, 3.0);
 
@@ -15,6 +15,8 @@ namespace TEST1
             s2 = SquateSolution.SolvedEcuation2(5.0, 2.0);
 
             System.Console.WriteLine("la solucion de s2 es: " + s2);
+
+            vector = geometry.GetVector
 
         }
     }
---
+++ TEST1/TEST1/geometry.cs
@@ -0,0 +1,15 @@
+﻿namespace TEST1
+{
+    class geometry
+    {
+        public static GetVector(double x, double y)
+        {
+            double aux, length;
+
+            aux = x * x + y * y;
+            length = System.Math.Sqrt(aux);
+
+            return length;
+        }
+    }
+}
