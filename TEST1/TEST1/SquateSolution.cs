d49d3bd9e5c7cd3e3af58cbb4814983aee1c50e0
Ricardohll <71719429+Ricardohll@users.noreply.github.com>
2020-09-29 19:38:12 +0200

    commit ecuacion
    creacion de una clase para ecuaciones

---
+++ TEST1/TEST1/SquateSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace TEST1
+{
+    class SquateSolution
+    {
+        public static double SolvedEcuation(double a, double b, double c)
+        {
+            double aux, aux2, x;
+
+            aux = b * b - 4.0 * a * c;
+            aux2 = System.Math.Sqrt(aux);
+            x = (-b + aux2) / (2.0 * a);
+
+            return x;
+
+        }
+
+        public static double SolvedEcuation2(double a2, double b2)
+        {
+
+        }
+    }
+}
