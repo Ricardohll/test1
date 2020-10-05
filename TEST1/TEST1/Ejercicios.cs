06892b0e6d41d6d0fa00756ea23a66cc67b939a3
Ricardohll <71719429+Ricardohll@users.noreply.github.com>
2020-10-05 17:22:08 +0200

    Create Ejercicios
    while

---
+++ TEST1/TEST1/Ejercicios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEST1
+{
+    class Ejercicios
+    {
+        //Ejercicio: Hacer una funcion que imprima por pantalla los numeros del 0 al 99
+
+        public static void Ejercicio()
+        {
+            int contador;
+            
+            contador = 0;
+
+            while (contador < 100)
+            {
+                System.Console.WriteLine(contador);
+                // contador = contador + 1;
+                // contador += 1;
+                contador++;
+
+            }
+        }
+    }
+}
